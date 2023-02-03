9a9478c99fb0b91edc7fafb5f384a7b41f524ddd
maxmwang <orangemax888@gmail.com>
2023-02-03 03:30:21 -0800

    Heroku deploy setup

--- app/server.ts
+++ app/server.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import * as dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
@@ -11,10 +12,18 @@ const app = express();
 const server = http.createServer(app);
 const io: Server = new Server(server);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve('client/dist')));
+}
+
 io.on('connection', async (socket) => {
   socket.on('message', (message) => {
     io.emit('message', message);
   });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('client/dist/index.html'));
 });
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
